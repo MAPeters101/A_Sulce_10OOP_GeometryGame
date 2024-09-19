8d91be9535511f144e3f250b37cb761e43ee7e96
Mark Peters <mapeters101@earthlink.net>
2024-09-19 15:39:33 -0400

    add falls_in_rectangle method to Point

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        print(self)
 
-point1 = Point(10, 20)
-point2 = Point(6, 7)
-print(point1.x)
-print(point2.y)
+
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] \
+            and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
+
+point1 = Point(5, 6)
+point2 = Point(7, 9)
+point3 = Point(3, 4)
+
+print(point3.falls_in_rectangle((5,6), (7,9)))
+# TypeError: 'Point' object is not subscriptable
+# print(point3.falls_in_rectangle(point1, point2))
+
+print(point3.falls_in_rectangle((1,1), (6,6)))
