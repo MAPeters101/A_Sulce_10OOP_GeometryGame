beddd5ed20b5fd20f2af9fd47bba8a17cfd01d74
Mark Peters <mapeters101@earthlink.net>
2024-09-20 14:09:57 -0400

    Create Rectangle class and chage accordingly.

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 class Point:
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-
-    def falls_in_rectangle(self, lowleft, upright):
-        if lowleft[0] < self.x < upright[0] \
-            and lowleft[1] < self.y < upright[1]:
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft.x < self.x < rectangle.upright.x \
+        and rectangle.lowleft.y < self.y < rectangle.upright.y:
             return True
         else:
             return False
 
-    def distance(self, x, y):
-        return ((self.x - x)**2 + (self.y - y)**2)**0.5
+class Rectangle:
 
-    def distance_from_point(self, otherPoint):
-        return ((self.x - otherPoint.x)**2 + (self.y - otherPoint.y)**2)**0.5
+    def __init__(self, lowleft, upright):
+        self.lowleft = lowleft
+        self.upright = upright
 
 
+pointx = Point(6,7)
+rectanglex = Rectangle(Point(5, 6), Point(7, 9))
 
-point1 = Point(5, 6)
-point2 = Point(7, 9)
-point3 = Point(3, 4)
-point4 = Point(8,10)
-
-print(point3.falls_in_rectangle((5,6), (7,9)))
-# TypeError: 'Point' object is not subscriptable
-# print(point3.falls_in_rectangle(point1, point2))
-
-print(point3.falls_in_rectangle((1,1), (6,6)))
-
-print(point1.distance(8, 10))
-print(point1.distance_from_point(point4))
+print(pointx.falls_in_rectangle(rectanglex))
